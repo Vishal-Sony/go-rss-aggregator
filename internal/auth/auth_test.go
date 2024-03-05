4d77dcce438e5c03a9bdb7cbcc50b970a269c8bc
Vishal Sony <75028186+Vishal-Sony@users.noreply.github.com>
2024-03-05 08:15:48 +0530

    CICD

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	header := http.Header{
+		"Authorization": []string{"ApiKey 1234dfghn"},
+	}
+	apiKey, err := GetAPIKey(header)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if apiKey != "1234dfghn" {
+		t.Errorf("Expected API key '1234dfghn', got %s", apiKey)
+	}
+
+	emptyHeaders := http.Header{}
+	_, err = GetAPIKey(emptyHeaders)
+	if err != errors.New("no authentication info found") {
+		t.Errorf("Expected Error, got %v", err)
+	}
+
+	malformedHeaders := http.Header{"Authorization": []string{"Bearer invalid-token"}}
+	_, err = GetAPIKey(malformedHeaders)
+	if err == nil || err.Error() != "malformed authorization header" {
+		t.Errorf("Expected 'malformed authorization header' error, got %v", err)
+	}
+}
